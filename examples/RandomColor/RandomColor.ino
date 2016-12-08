eb1880658e7ca036af86e64ee8d2452f0e0688d5
Richard Wardlow <richard@circuitsforfun.com>
2016-12-07 18:43:01 -0800

    Update RandomColor.ino

--- examples/RandomColor/RandomColor.ino
+++ examples/RandomColor/RandomColor.ino
@@ -37,7 +37,7 @@ void loop() {
   {
       Serial.println("Button1 Pressed");
       int rndNum = random(267, 65534);
-      myHue.setGroup(0, myHue.ON, 255, 255, rndNum);   // Set group 0 to random color (group 0 is all lights be default)
+      myHue.setGroup(0, myHue.ON, 255, 255, rndNum);   // Set group 0 to random color (group 0 is all lights by default)
       delay(100);
   }
 }
